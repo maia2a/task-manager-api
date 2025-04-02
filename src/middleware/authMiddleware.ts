cba9d67ed227db73d79b9fb397c27fd0a3693df5
Gabriell Maia <gabrielldeveloper@gmail.com>
2025-04-02 10:02:39 -0300

    feat: pacotes novos e arquivo de authMiddleware

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+
+  } catch (error) {
+
+  }
+};
